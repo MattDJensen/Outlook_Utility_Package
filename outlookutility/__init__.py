b24a75899351b58eaa0761611329ff88e8e2be03
Matthew Jensen <matt4759@gmail.com>
2020-07-20 13:51:44 -0400

    1.3 Release
    Add Index paramter to default_table_style

--- outlookutility/__init__.py
+++ outlookutility/__init__.py
@@ -6,4 +6,4 @@ from .email_with_attachments import email_with_attachments
 from .notify_error import notify_error
 from .default_table_style import default_table_style
 # Version of the package
-__version__ = "1.2"
+__version__ = "1.3"
